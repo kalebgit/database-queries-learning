c977c296bf8466eb5ea6eb3e4c199f4284a1b60f
Jimenez Rivera Emiliano Kaleb <emilianokaleb@gmail.com>
2024-02-08 07:53:06 -0600

    feat(): new queries

---
+++ mySql/firstQueries.sql
@@ -0,0 +1,23 @@
+#CREATE DATABASE myDB;
+#USE myDB;
+#DROP DATABASE myDB;
+#CREATE DATABASE myDB;
+-- USE myDB;
+-- CREATE TABLE employees(
+-- 	employee_id INT, 
+--     email VARCHAR(40),
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50), 
+--     hourly_pay DECIMAL(2),
+--     hire_date DATE
+-- )
+-- SELECT * FROM employees
+-- RENAME TABLE employees TO workers
+-- ALTER TABLE workers 
+-- ADD phoneNumber VARCHAR(15)
+-- ALTER TABLE workers RENAME COLUMN phoneNumber TO houseNumber
+-- ALTER TABLE workers
+-- MODIFY COLUMN houseNumber VARCHAR(100)
+-- ALTER TABLE workers
+-- MODIFY hire_date DATE
+-- AFTER last_name
